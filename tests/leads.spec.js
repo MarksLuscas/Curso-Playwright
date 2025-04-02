52e4caef90bf23fbbc2b709fac45c8a02461e241
Lucas Marques <lucas99marques@gmail.com>
2025-04-02 12:13:23 -0300

    Adicionando mais cenários

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -25,10 +25,82 @@ test('deve cadastrar um lead na tela de espera', async ({ page }) => {
 
     estrategia para conseguir validar o elemento que aparece na tela de forma muito rapida, pegando o conteudo html da pagina no momento que esse elemento aparece
 */ 
-
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
   await expect(page.locator('.toast')).toHaveText(message)
 
-
   await expect(page.locator('.toast')).toBeHidden({timeout:5000}) // elemento tem até 5seg para ficar invisivel
+});
+
+
+test('nao deve cadastrar com um email incorreto', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', {name: /Aperte o play/}).click() // -> dessa forma o localizador está buscando algum botao que tenha Aperte o play em algum texto dele
+   
+  expect(page.getByTestId('modal')
+            .getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Lucas')
+  await page.getByPlaceholder('Seu email principal').fill('lucas.com.br') // -> ja busca direto pelo valor de placeholder
+
+  await page.getByTestId('modal')
+      .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')    
+});
+
+
+
+
+
+test('validar campo Nome obrigatorio', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', {name: /Aperte o play/}).click() // -> dessa forma o localizador está buscando algum botao que tenha Aperte o play em algum texto dele
+   
+  expect(page.getByTestId('modal')
+            .getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu email principal').fill('lucas@yahoo.com.br') // -> ja busca direto pelo valor de placeholder
+
+  await page.getByTestId('modal')
+      .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')    
+});
+
+
+
+test('validar campo Email obrigatorio', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', {name: /Aperte o play/}).click() // -> dessa forma o localizador está buscando algum botao que tenha Aperte o play em algum texto dele
+   
+  expect(page.getByTestId('modal')
+            .getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Lucas')
+
+  await page.getByTestId('modal')
+      .getByText('Quero entrar na fila!').click()
+
+      await expect(page.locator('.alert')).toHaveText('Campo obrigatório')    
+});
+
+
+test('validar campos obrigatorios', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', {name: /Aperte o play/}).click() // -> dessa forma o localizador está buscando algum botao que tenha Aperte o play em algum texto dele
+   
+  expect(page.getByTestId('modal')
+            .getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByTestId('modal')
+      .getByText('Quero entrar na fila!').click()
+
+      await expect(page.locator('.alert')).toHaveText([
+        'Campo obrigatório',
+        'Campo obrigatório'
+      ])    // validar duas instancias diferentes recebendo/apresentando essa mesma mensagem. Sem dar erro de ambiguidade
 });
