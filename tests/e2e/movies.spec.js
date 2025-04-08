51d6f8db212fd735e8e0c483ae22e108df3d440f
Lucas Marques <lucas99marques@gmail.com>
2025-04-08 17:50:29 -0300

    Adicionando ligação com Banco de Dados

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,8 +1,9 @@
 const { test } = require('@playwright/test')
 const data = require('../support/fixtures/movies.json')
+const { executeSQL } = require('../support/database')
 const { LoginPage } = require('../pages/loginPage')
-const {Toast} = require('../pages/components')
-const {MoviesPage} = require('../pages/MoviesPage')
+const { Toast } = require('../pages/components')
+const { MoviesPage } = require('../pages/MoviesPage')
 
 
 let loginPage
@@ -16,17 +17,19 @@ test.beforeEach(({ page }) => {
 })
 
 
-test('deve poder cadastrar um novo filme', async ({page}) =>{
+test('deve poder cadastrar um novo filme', async ({ page }) => {
 
     const movie = data.create
+
+    await executeSQL(`DELETE from movies where title = '${movie.title}'; `)
 
     await loginPage.visit()
     await loginPage.fillForms("admin@zombieplus.com", "pwd123")
     await moviesPage.isLogged()
 
-    await moviesPage.create()
+    await moviesPage.create(movie.title, movie.overview, movie.company, movie.release_year)
 
 
-    await Toast.containText('Cadastro realizado com sucesso!')
+    await toast.containText('Cadastro realizado com sucesso!')
 
 })
